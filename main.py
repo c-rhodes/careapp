5e28a6b1d9b8247919243c13222eb8befe9d756c
Cullen Rhodes <rhodes.cullen@yahoo.co.uk>
2015-02-12 21:22:43 +0000

    Latest

--- main.py
+++ main.py
@@ -3,19 +3,14 @@ __version__ = '0.0.1'
 import kivy
 
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-
-from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.listview import ListView, ListItemButton
-from kivy.adapters.simplelistadapter import SimpleListAdapter
-from kivy.adapters.listadapter import ListAdapter
-from kivy.uix.label import Label
+from kivy.uix.listview import ListItemButton
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty, StringProperty, ListProperty
+from kivy.utils import get_color_from_hex
 
-DISABILITIES = [dict(text='Disability {}'.format(i), is_selected=False)  for i in range(100)]
+DISABILITIES = ['Disability {}'.format(i)  for i in range(100)]
+
 
 class UserSelectScreen(Screen):
     pass
@@ -26,41 +21,30 @@ class DisabilitiesScreen(Screen):
 
     def __init__(self, **kwargs):
         super(DisabilitiesScreen, self).__init__(**kwargs)
-    
-        # simple_list_adapter = SimpleListAdapter(
-        #     data=["Item #{0}".format(i) for i in range(100)],
-        #     cls=Label)
-        # self.list_view = ListView(adapter=simple_list_adapter)
-    
-        # self.list_adapter = ListAdapter(
-        #     data=DISABILITIES,
-        #     args_converter=self.args_converter,
-        #     cls=ListItemButton,
-        #     selection_mode='single',
-        #     allow_empty_selection=False
-        # )
-        # self.list_view = ListView(adapter=self.list_adapter)
-        # self.add_widget(self.list_view)
-
-    def on_list_view(self, instance, view):
-        print('on_list_view')
-        if view:
-            view.adapter.data = DISABILITIES
-
-    def args_converter(self, row_index, obj):
-        return dict(text=obj['text'], size_hint_y=None, height=25)
 
     def disability_converter(self, index, disability):
         result = dict(name=disability)
         return result
 
 
-class DisabilityListItem(BoxLayout):
+class DisabilityInfoScreen(Screen):
+    pass
+
+class DisabilityListItem(ListItemButton):
     name = StringProperty()
 
 
 class CareAppScreenManager(ScreenManager):
-    pass
+
+    def __init__(self, **kwargs):
+        super(CareAppScreenManager, self).__init__(**kwargs)
+
+    def new_disability_info_screen(self, disability):
+        if not self.has_screen(disability):
+            s = DisabilityInfoScreen(name=disability)
+            self.add_widget(s)
+
+        self.current = disability
 
 
 class CareApp(App):
