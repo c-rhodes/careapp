9186426e0822b8ff47eee431b35bcb392b94ce59
Cullen Rhodes <rhodes.cullen@yahoo.co.uk>
2015-03-02 14:12:02 +0000

    Added initial services data and changed app code from screens to standard layouts with management code.

--- main.py
+++ main.py
@@ -4,52 +4,62 @@ import kivy
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.listview import ListItemButton
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.listview import ListItemButton, ListView
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty, StringProperty, ListProperty
 from kivy.utils import get_color_from_hex
+from kivy.adapters.listadapter import ListAdapter
 
 DISABILITIES = ['Disability {}'.format(i)  for i in range(100)]
+services = {
+    'carer': ['Alzheimer\'s Society', 'Carer\'s Reablement'],
+    'caree': ['test', 'test2']
+}
+
+class UserSelectForm(FloatLayout):
+
+    def show_services_list(self, user):
+        print(user)
+        self.clear_widgets()
+        self.services_list = ServiceList(user)
+        self.add_widget(self.services_list)
 
 
-class UserSelectScreen(Screen):
-    pass
-
-
-class DisabilitiesScreen(Screen):
+class ServiceList(BoxLayout):
     list_view = ObjectProperty()
 
-    def __init__(self, **kwargs):
-        super(DisabilitiesScreen, self).__init__(**kwargs)
+    def __init__(self, user, **kwargs):
+        super(ServiceList, self).__init__(**kwargs)
+        self.app = CareApp.get_running_app()
+        self.list_view.adapter.data.extend(services[user])
 
     def disability_converter(self, index, disability):
         result = dict(name=disability)
         return result
 
+    def show_userselect(self):
+        self.clear_widgets()
+        self.add_widget(UserSelectForm()) 
 
-class DisabilityInfoScreen(Screen):
-    pass
-
-class DisabilityListItem(ListItemButton):
+class ServiceListItem(ListItemButton):
     name = StringProperty()
 
+class ServiceInfoPage(BoxLayout):
+    def __init__(self, name, **kwargs):
+        self.name = name
+        super(ServiceInfoPage, self).__init__(**kwargs)
 
-class CareAppScreenManager(ScreenManager):
 
-    def __init__(self, **kwargs):
-        super(CareAppScreenManager, self).__init__(**kwargs)
+class CareAppRoot(BoxLayout):
 
-    def new_disability_info_screen(self, disability):
-        if not self.has_screen(disability):
-            s = DisabilityInfoScreen(name=disability)
-            self.add_widget(s)
-
-        self.current = disability
-
+    def new_service_info_page(self, service):
+        self.service_info_page = ServiceInfoPage(service)
+        self.clear_widgets()
+        self.add_widget(self.service_info_page)
 
 class CareApp(App):
-    pass
-
+    pass    
 
 if __name__ == '__main__':
     CareApp().run()
